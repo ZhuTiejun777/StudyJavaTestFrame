d9ef7fadf10c953a6329162d843f2262d1a8d960
zhutj <zhutiejun777@163.com>
2020-05-27 10:12:31 +0800

    perfect apiauto

--- apiauto/src/main/java/com/qa/tests/GetApiTest.java
+++ apiauto/src/main/java/com/qa/tests/GetApiTest.java
@@ -4,57 +4,58 @@ import java.io.IOException;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.qa.base.TestBase;
 import com.qa.restclient.RestClient;
 import com.qa.util.TestUtil;
 
-public class GetApiTest extends TestBase{
+public class GetApiTest extends TestBase {
+
     TestBase testBase;
     String host;
     String url;
     RestClient restClient;
     CloseableHttpResponse closeableHttpResponse;
 
+    final static Logger Log = Logger.getLogger(GetApiTest.class);
 
     @BeforeClass
     public void setUp() {
+
         testBase = new TestBase();
+        //Log.info("测试服务器地址为："+ host.toString());
         host = prop.getProperty("HOST");
+        //Log.info("当前测试接口的完整地址为："+url.toString());
         url = host + "/api/users?page=2";
 
     }
 
     @Test
     public void getAPITest() throws ClientProtocolException, IOException {
+        Log.info("开始执行用例...");
         restClient = new RestClient();
         closeableHttpResponse = restClient.get(url);
 
         //断言状态码是不是200
-        int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
+        Log.info("测试响应状态码是否是200");
+        int statusCode = restClient.getStatusCode(closeableHttpResponse);
         Assert.assertEquals(statusCode, RESPNSE_STATUS_CODE_200, "response status code is not 200");
 
-        //把响应内容存储在字符串对象
-        String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(),"UTF-8");
-
-        //创建Json对象，把上面字符串序列化成Json对象
-        JSONObject responseJson = JSON.parseObject(responseString);
+        JSONObject responseJson = restClient.getResponseJson(closeableHttpResponse);
         //System.out.println("respon json from API-->" + responseJson);
 
         //json内容解析
-        String s1 = TestUtil.getValueByJPath(responseJson,"data[0]/first_name");
-        String s2 = TestUtil.getValueByJPath(responseJson,"data[1]/id");
-        String s3 = TestUtil.getValueByJPath(responseJson,"per_page");
-        System.out.println(s1);
-        System.out.println(s2);
-        System.out.println(s3);
-        //常用测试断言,实际结果,预期结果,断言失败打印日志
-        Assert.assertEquals(s1, "Michael", "first name is Michael");
+        String s = TestUtil.getValueByJPath(responseJson,"data[0]/first_name");
+        Log.info("执行JSON解析，解析的内容是 " + s);
+        //System.out.println(s);
+        Log.info("接口内容响应断言");
+        Assert.assertEquals(s, "Michael","first name is not Eve");
+        Log.info("用例执行结束...");
     }
+
+
 }
