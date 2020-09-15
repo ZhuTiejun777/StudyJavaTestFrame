2ac517ad12f39e2698c0b7855c46e962a81764ec
zhutj <zhutiejun777@163.com>
2020-09-15 10:45:12 +0800

    20200915

--- StudyFrame/src/main/java/frame/study/JDBCStudy/JDBCDemo1.java
+++ StudyFrame/src/main/java/frame/study/JDBCStudy/JDBCDemo1.java
@@ -1,6 +1,24 @@
 package frame.study.JDBCStudy;
 
+import java.sql.*;
+
 public class JDBCDemo1 {
 
+    public static void main(String[] args) throws SQLException {
 
+        DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@192.168.3.55:1521:zjporttest", "etctest", "etctest");
+        Statement statement = connection.createStatement();
+        // ResultSet resultSet = statement.executeQuery("select vehicle_code from etc_card");
+        ResultSet resultSet = statement.executeQuery("select count(1) from etc_card");
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getObject(1));
+        }
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+    }
 }
