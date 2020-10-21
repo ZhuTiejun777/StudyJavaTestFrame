44c67896d70183d78ba006d0c79b9a315873d445
zhutj <zhutiejun777@163.com>
2020-10-21 14:22:41 +0800

    20201021

---
+++ UiAuto/src/main/java/com/qa/practice/test.java
@@ -0,0 +1,23 @@
+package com.qa.practice;
+
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("b","4");
+        map.put("a","5");
+        map.put("c","3");
+        map.put("d","5");
+        System.out.println(map);
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+        System.out.println(map.get("b"));
+        System.out.println();
+        for (String key : map.keySet()) {
+            System.out.println(map.get(key));
+        }
+
+
+    }
+}
